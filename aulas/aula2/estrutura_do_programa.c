2f0cc7750a8bb1825baec150fb422b72924ff828
Marcos <marcospedrozadigital@gmail.com>
2025-08-20 21:52:46 -0300

    codigos da aula2

---
+++ aulas/aula2/estrutura_do_programa.c
@@ -0,0 +1,10 @@
+#include <st
+dio.h>
+int main () {}
+1.Entrada
+2.Processamento
+3. Saída
+
+return 0;
+
+
---
+++ aulas/aula2/funcao_printf.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main () {
+    setlocale (LC_ALL, "");
+    printf ("Olá Pessoal\n");    
+    printf ("Em que posso ajudar?\n");
+    printf ("Gere minha nota legal\n");
+    printf ("=====================================/n");
+    printf ("NOTA LEGAL\n");
+printf ("Carderno 001 50,00\n");
+printf ("Lápis 002 7,50 \n");
+printf ("Borracha 003 12,00 \n");
+printf ("Total. 102,50\n");
+ printf ("%15s %3i %7.2f \n," "Lápis", 3 11.75);
+    
+    \\ especificador de formato
+    \\ %c imprimir um caracteer
+    \\ %s imprimir um text (string)
+    \\ %i imprimir um inteiro
+    \\ %f imprimir um funcionario (flutuante)
+
+
+
+
+
+    return 0;
+    
+}
